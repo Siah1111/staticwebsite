ce8c315d3856d1eddb31ae8d88f5a8f11ca0b5a1
Josiah Naisbitt <josiahnaisbitt@my.unt.edu>
2025-10-26 17:53:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://youtu.be/lCAVcUQY83c?si=AhZBpGnAnSKdWpG7" target="_blank">
           <img src={angle} className="logo" alt="Angle" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://youtu.be/N5dTcc1eS4w?si=iKO2FvSBGqfu75Np" target="_blank">
           <img src={devil} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>I could be your angle or your devil</h1>
       <p className="read-the-docs">
-        I could be your angle or your devil
+        A simple website by Josiah Naisbitt.
+        CSCE 3420.401
       </p>
     </>
   )
